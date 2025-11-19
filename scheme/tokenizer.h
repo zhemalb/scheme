5baf640cfedd3072dd870a2061be862a51d11be4
zhemalb <aazhemukov@edu.hse.ru>
2025-11-19 16:18:57 +0300

    tokenizer

--- scheme/tokenizer.h
+++ scheme/tokenizer.h
@@ -1,41 +1,85 @@
 #pragma once
 
-#include <variant>
+#include <cstddef>
 #include <istream>
 #include <string>
+#include <variant>
+#include <vector>
 
 struct SymbolToken {
     std::string name;
-
-    bool operator==(const SymbolToken& other) const;
+    bool operator==(const SymbolToken& other) const {
+        return name == other.name;
+    }
 };
 
 struct QuoteToken {
-    bool operator==(const QuoteToken&) const;
+    bool operator==(const QuoteToken&) const {
+        return true;
+    }
 };
 
 struct DotToken {
-    bool operator==(const DotToken&) const;
+    bool operator==(const DotToken&) const {
+        return true;
+    }
 };
 
 enum class BracketToken { OPEN, CLOSE };
 
 struct ConstantToken {
-    int value;
-
-    bool operator==(const ConstantToken& other) const;
+    int64_t value;
+    bool operator==(const ConstantToken& other) const {
+        return value == other.value;
+    }
 };
 
 using Token = std::variant<ConstantToken, BracketToken, SymbolToken, QuoteToken, DotToken>;
 
-// Интерфейс, позволяющий читать токены по одному из потока.
 class Tokenizer {
 public:
-    Tokenizer(std::istream* in);
+    Tokenizer(std::istream* in) {
+        in_ = std::move(in);
+        tokens_ = {};
+        pos_  = 0;
+        end_reached_ = false;
+    }
 
-    bool IsEnd();
+    bool IsEnd() {
+        return !EnsureToken(pos_);
+    }
 
-    void Next();
+    void Next() {
+        if (!IsEnd()) {
+            ++pos_;
+        }
+    }
 
-    Token GetToken();
+    Token GetToken() {
+        if (!EnsureToken(pos_)) {
+            return SymbolToken{""};
+        }
+        return tokens_[pos_];
+    }
+
+private:
+    using TokenList = std::vector<Token>;
+
+    std::istream* in_ = nullptr;
+    TokenList tokens_;
+    size_t pos_ = 0;
+    bool end_reached_ = false;
+
+    bool ReadToken(Token& out);
+    bool EnsureToken(size_t index) {
+        while (tokens_.size() <= index && !end_reached_) {
+            Token token;
+            if (ReadToken(token)) {
+                tokens_.push_back(std::move(token));
+            } else {
+                end_reached_ = true;
+            }
+        }
+        return index < tokens_.size();
+    }
 };
